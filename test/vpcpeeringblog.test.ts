4b6199159304835d2294d5944d07f94986c79458
John Ruffin <john@johnruffin.io>
2023-09-30 12:04:29 -0500

    Initial commit

---
+++ test/vpcpeeringblog.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Vpcpeeringblog from '../lib/vpcpeeringblog-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/vpcpeeringblog-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Vpcpeeringblog.VpcpeeringblogStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
